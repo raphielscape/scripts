4f15f905f43333540cf8d7cb859832c9ad4dd8ea
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-07-11 22:35:12 +0700

    SRV: path: inline
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- configure-srv.sh
+++ configure-srv.sh
@@ -5,7 +5,7 @@
 source "${HOME}/scripts/env"
 
 # Export Kernel Directory
-export KERNELDIR=${HOME}/weeb_kernel_msm8953
+export KERNELDIR=${HOME}/kat_kernel_reworks
 
 # Some alias
 function clone() {
